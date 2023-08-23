7f795b1810299ee200d401f5635d61f7fee47537
sergey4028 <msp4028@yandex.ru>
2023-08-23 13:54:22 +0300

    tasks

---
+++ task2.py
@@ -0,0 +1,14 @@
+a=(input("Введите 4-значное число:"))
+n0=a[0]
+n1=a[1]
+n2=a[2]
+n3=a[3]
+n0=int(n0)
+n1=int(n1)
+n2=int(n2)
+n3=int(n3)
+a=n0+n2
+print(a)
+
+b=n1-n3
+print(b)
---
+++ task9.py
@@ -0,0 +1,7 @@
+a=int((input("Введите число:")))
+
+# while  type(a) in (int, float, bool) ==True :  a=input("Введите число:")
+
+if a%2 ==0 : print ("четное") 
+else : print("нечетное")
+
